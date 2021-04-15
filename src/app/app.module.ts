6985daf1c00593ea7cf63e87c0e189bacbff5b50
youssef <youss.hark@gmail.com>
2021-04-14 23:05:42 -0400

    Started catalog app layout and config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { ProductsComponent } from './components/products/products.component';
+import { HomeComponent } from './components/home/home.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProductsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/products.services.ts
@@ -0,0 +1,16 @@
+import {Injectable} from "@angular/core";
+import {HttpClient} from "@angular/common/http";
+import {environment} from "../../environments/environment";
+import {Observable} from "rxjs";
+import {Product} from "../Models/product.model";
+
+@Injectable({providedIn: "root"})
+export class ProductsService {
+  constructor(private http:HttpClient) {
+  }
+
+  getAllProducts(): Observable<Product[]>{
+    let host=environment.host;
+    return this.http.get<Product[]>(host+"/products");
+  }
+}

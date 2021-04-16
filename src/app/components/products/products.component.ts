177a1e1675ed746badd5e28b1c18758fd7486f55
youssef <youss.hark@gmail.com>
2021-04-16 15:55:47 -0400

    Added search delete update functions

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -4,6 +4,7 @@ import {Product} from "../../Models/product.model";
 import {Observable, of} from "rxjs";
 import {catchError, map, startWith} from "rxjs/operators";
 import {AppDataState, DataStateEnum} from "../../state/product.state";
+import {dashCaseToCamelCase} from "@angular/compiler/src/util";
 
 @Component({
   selector: 'app-products',
@@ -26,5 +27,49 @@ export class ProductsComponent implements OnInit {
         startWith({dataState:DataStateEnum.LOADING}),
         catchError(err => of({dataState:DataStateEnum.ERROR, errMessage:err.message}))
       );
+  }
+
+  getSelectedProducts() {
+    this.products$ =
+      this.productService.getSelectedProducts().pipe(
+        map(data=>({dataState:DataStateEnum.LOADED,data:data})),
+        startWith({dataState:DataStateEnum.LOADING}),
+        catchError(err => of({dataState:DataStateEnum.ERROR, errMessage:err.message}))
+      );
+  }
+
+  getAvalaibleProducts() {
+    this.products$ =
+      this.productService.getAvalaibleProducts().pipe(
+        map(data=>({dataState:DataStateEnum.LOADED,data:data})),
+        startWith({dataState:DataStateEnum.LOADING}),
+        catchError(err => of({dataState:DataStateEnum.ERROR, errMessage:err.message}))
+      );
+  }
+
+  onSearch(dataForm: any) {
+    this.products$ =
+      this.productService.searchProducts(dataForm.keyword).pipe(
+        map(data=>({dataState:DataStateEnum.LOADED,data:data})),
+        startWith({dataState:DataStateEnum.LOADING}),
+        catchError(err => of({dataState:DataStateEnum.ERROR, errMessage:err.message}))
+      );
+  }
+
+  onSelect(p: Product) {
+    this.productService.select(p)
+      .subscribe(data=> {
+        p.selected=data.selected;
+      })
+  }
+
+  onDelete(p: Product) {
+    let v=confirm("Do you want to delete "+p.name+" ?");
+    if(v==true) {
+      this.productService.delete(p)
+        .subscribe(data => {
+          this.getAllProducts();
+        })
+    }
   }
 }
--- src/app/services/products.services.ts
+++ src/app/services/products.services.ts
@@ -4,23 +4,36 @@ import {environment} from "../../environments/environment";
 import {Observable} from "rxjs";
 import {Product} from "../Models/product.model";
 
+
+let host = environment.host;
+
 @Injectable({providedIn: "root"})
 export class ProductsService {
   constructor(private http:HttpClient) {
   }
 
   getAllProducts(): Observable<Product[]>{
-    let host=environment.host;
     return this.http.get<Product[]>(host+"/products");
   }
 
   getSelectedProducts(): Observable<Product[]>{
-    let host=environment.host;
     return this.http.get<Product[]>(host+"/products?selected=true");
   }
 
   getAvalaibleProducts(): Observable<Product[]>{
-    let host=environment.host;
     return this.http.get<Product[]>(host+"/products?available=true");
+  }
+
+  searchProducts(keyword:string): Observable<Product[]> {
+    return this.http.get<Product[]>(host + "/products?name_like="+keyword);
+  }
+
+  select(product: Product): Observable<Product> {
+    product.selected = !product.selected;
+    return this.http.put<Product>(host + "/products/"+product.id, product);
+  }
+
+  delete(product: Product): Observable<void> {
+    return this.http.delete<void>(host + "/products/"+product.id);
   }
 }
